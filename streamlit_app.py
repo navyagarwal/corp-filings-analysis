d43f68aa731109626baa3634f2d8930e9b25f0bb
Navya Agarwal <82928853+navyagarwal@users.noreply.github.com>
2024-05-02 18:01:26 +0000

    Add bar chart visualization

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,20 @@ import json
 import streamlit.components.v1 as components
 from sec_edgar_downloader import Downloader as SecEdgarDownloader
 from sec_downloader.download_storage import DownloadStorage
-from utility.functions import getSummary
+from utility.functions import getSummary, getFigure, getYear, cleanFigures
 
-ticker_title_file_path = "utility/ticker_title_dict.json"
-with open(ticker_title_file_path, 'r') as file:
-    ticker_title_dict = json.load(file)
+retail_company_tickers_file_path = "utility/retail_company_tickers.json"
+with open(retail_company_tickers_file_path, 'r') as file:
+    retail_company_tickers_dict = json.load(file)
 
 with st.sidebar:
     option = st.selectbox(
-        'Company Name',
-        list(ticker_title_dict.values())
+        '&nbsp; Select Company Name',
+        list(retail_company_tickers_dict.values())
     )
-    ticker = [key for key, value in ticker_title_dict.items() if value == option]
+    ticker = [key for key, value in retail_company_tickers_dict.items() if value == option]
     if ticker:
-        st.write('Ticker:', ticker[0])
+        st.write('&nbsp; Ticker:', ticker[0])
         storage = DownloadStorage()
         with storage as path:
             dl = SecEdgarDownloader("MyCompanyName", "email@example.com", path)
@@ -25,6 +25,22 @@ with st.sidebar:
     else:
         st.write('No ticker found for the selected company')
 
+
+st.title(retail_company_tickers_dict[ticker[0]])
+st.divider()
+st.subheader("Summary of Risk Factors associated with the Company")
 raw_10k = storage.get_file_contents()[0].content
-summary = getSummary(raw_10k)
+summary = getSummary(raw_10k, 1)
 st.write(summary)
+
+dict = {}
+for path, content in storage.get_file_contents():
+    year = getYear(path)
+    figure = getFigure(content, year)
+    dict[year] = figure
+
+st.markdown('#')
+st.markdown('<h5 style="text-align: center;">Trend of Earnings Per Share over the last 5 Years</h5>', unsafe_allow_html=True)
+# st.caption('Trend of Earnings Per Share over the last 5 Years')
+dict = cleanFigures(dict)
+st.bar_chart(dict, height=300)
--- utility/functions.py
+++ utility/functions.py
@@ -3,6 +3,8 @@ import os
 import re
 import pandas as pd
 import requests
+import string
+import numpy as np
 from bs4 import BeautifulSoup
 
 
@@ -18,11 +20,11 @@ def _create_ticker_title_dict(json_file_path):
         json.dump(ticker_title_dict, outfile, indent=4)
     print(f"Ticker:title dictionary created and saved as {output_file_path}")
 
-def getSummary(raw_10k):
-    section7Text = _getSection7Text(raw_10k)
-    return _summarize(section7Text)
+def getSummary(raw_10k, item):
+    sectionText = _getSectionText(raw_10k, item)
+    return _summarize(sectionText)
 
-def _getSection7Text(raw_10k):
+def _getSectionText(raw_10k, item):
     doc_start_pattern = re.compile(r'<DOCUMENT>')
     doc_end_pattern = re.compile(r'</DOCUMENT>')
     type_pattern = re.compile(r'<TYPE>[^\n]+')
@@ -33,23 +35,47 @@ def _getSection7Text(raw_10k):
     for doc_type, doc_start, doc_end in zip(doc_types, doc_start_is, doc_end_is):
         if doc_type == '10-K':
             document[doc_type] = raw_10k[doc_start:doc_end]
-    regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(7|7A)\.{1})|(ITEM\s(7|7A)\.{1})')
+    regex = re.compile(r'(>Item(\s|&#160;|&nbsp;)(1A|1B|7|7A)\.{1})|(ITEM\s(1A|1B|7|7A)\.{0, 1})|(Item(\s|&#160;|&nbsp;)(1A|1B|7|7A)\s(-|—))|(Item(\s|&#160;|&nbsp;)(1A|1B|7|7A)(-|—))')
     matches = regex.finditer(document['10-K'])
     test_df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
     test_df.columns = ['item', 'start', 'end']
     test_df['item'] = test_df.item.str.lower()
+    # print(test_df)
     test_df.replace('&#160;',' ',regex=True,inplace=True)
     test_df.replace('&nbsp;',' ',regex=True,inplace=True)
     test_df.replace(' ','',regex=True,inplace=True)
     test_df.replace('\.','',regex=True,inplace=True)
     test_df.replace('>','',regex=True,inplace=True)
+    test_df.replace('-','',regex=True,inplace=True)
+    test_df.replace('—','',regex=True,inplace=True)
     pos_dat = test_df.sort_values('start', ascending=True).drop_duplicates(subset=['item'], keep='last')
     pos_dat.set_index('item', inplace=True)
-    item_7_raw = document['10-K'][pos_dat['start'].loc['item7']:pos_dat['start'].loc['item7a']]
-    item_7_content = BeautifulSoup(item_7_raw, 'lxml')
-    return item_7_content.get_text("\n\n")[0:]
+    # print(pos_dat)
+    
+    if(item == 1):
+        if(pos_dat['start'].loc['item1a'] < pos_dat['start'].loc['item1b']):
+            item_1_raw = document['10-K'][pos_dat['start'].loc['item1a']:pos_dat['start'].loc['item1b']]
+        else:
+            item_1_raw = document['10-K'][pos_dat['start'].loc['item1b']:pos_dat['start'].loc['item1a']]    
+        item_content = item_1_raw     
+        item_content = BeautifulSoup(item_content, 'lxml').get_text("\n\n")[0:]
+        item_content = re.sub(r'\d+', '', item_content)
+        item_content = "".join([char for char in item_content if char not in "#$%&()*+,/:;<=>?@[\]^_`{|}~"]) 
+        item_content = re.sub('\s+', ' ', item_content).strip()
+    
+    elif(item == 7):
+        if(pos_dat['start'].loc['item7'] < pos_dat['start'].loc['item7a']):
+            item_7_raw = document['10-K'][pos_dat['start'].loc['item7']:pos_dat['start'].loc['item7a']]
+        else:
+            item_7_raw = document['10-K'][pos_dat['start'].loc['item7a']:pos_dat['start'].loc['item7']]   
+        item_content = item_7_raw
+        item_content = BeautifulSoup(item_content, 'lxml').get_text("\n\n")[0:]
+        item_content = "".join([char for char in item_content if char not in "#&()*+,/:;<=>?@[\]^_`{|}~"]) 
+        item_content = re.sub('\s+', ' ', item_content).strip()
+        
+    return item_content
 
-def _summarize(section7Text):
+def _summarize(sectionText):
     API_TOKEN = "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"
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
@@ -58,8 +84,53 @@ def _summarize(section7Text):
         return response.json()
     data = query(
         {
-            "inputs": section7Text,
+            "inputs": sectionText,
             "parameters": {"do_sample": False},
+            "options": {"wait_for_model": True}
         }
     )
     return data[0]['summary_text']
+
+def getYear(path):
+    path = str(path)
+    yearTerm = path.split('/')[3]
+    year = yearTerm.split('-')[1]
+    year = "20"+year
+    return year
+
+def getFigure(raw_10k, year):
+    section7Text = _getSectionText(raw_10k, 7)
+    API_TOKEN = "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"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    API_URL = "https://api-inference.huggingface.co/models/deepset/roberta-base-squad2"
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+    data = query(
+        {
+            "inputs": {
+                "question": f"How much is the earnings per share in {year}?",
+                "context": section7Text,
+            },
+            "options": {"wait_for_model": True}
+        }
+    )
+    print(data)
+    return data["answer"]
+
+
+def cleanFigures(dict):
+    for key, value in dict.items():
+        value = value.replace('-', ' ')
+        list = value.split(' ')
+        list = [x for x in list if not x.isalpha()]
+        try:
+            value = list[-1]
+            value = value.replace('$', '')
+        except:
+            value = np.nan
+        if(float(value) > 40):
+            value = np.nan
+        dict[key] = value
+    
+    return dict
